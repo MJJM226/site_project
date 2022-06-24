43709135c4544db50af622b1b6ebd15d7bb5b831
MJJM226 <93908797+MJJM226@users.noreply.github.com>
2022-06-24 07:15:00 +0400

    call the account menu

---
+++ js/invisible-blocks.js
@@ -0,0 +1,7 @@
+akk.onclick = function() {
+    document.getElementsByClassName('invisible-blocks-akk')[0].style.zIndex = 99;
+}
+
+akk2.onclick = function() {
+    document.getElementsByClassName('invisible-blocks-akk')[0].style.zIndex = -1;
+}
